04be316ef2a0ecfa4d86c45ba8bd1890bf3071a4
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2019-06-13 13:03:56 +0600

    Updates

---
+++ precache-manifest.8d59bd0fa0c7a575be6e4b72cc5c1972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/OrderBurgerCart/static/media/127 burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "f731ba74a4831c4f7bd7",
+    "url": "/OrderBurgerCart/static/js/runtime~main.2552a775.js"
+  },
+  {
+    "revision": "5d004a20558c11edf694",
+    "url": "/OrderBurgerCart/static/js/main.04ae113d.chunk.js"
+  },
+  {
+    "revision": "aecd56cd4f395d5ad7f2",
+    "url": "/OrderBurgerCart/static/js/2.17fe79c6.chunk.js"
+  },
+  {
+    "revision": "5d004a20558c11edf694",
+    "url": "/OrderBurgerCart/static/css/main.c88bf2bc.chunk.css"
+  },
+  {
+    "revision": "dd79afd8fd72809652e2de4fbace7699",
+    "url": "/OrderBurgerCart/index.html"
+  }
+];
